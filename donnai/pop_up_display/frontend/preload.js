96e5e8ce7ee57dc52978f8645b1bb38361cfa22b
eve314 <eve.edah.devine@gmail.com>
2026-05-29 21:22:11 +0100

    Enhance preload.js with music generation IPC channels

--- donnai/pop_up_display/frontend/preload.js
+++ donnai/pop_up_display/frontend/preload.js
@@ -1,5 +1,56 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  // Existing functionality
   sendEvent: (eventData) => ipcRenderer.send('event-detected', eventData),
+
+  // Music generation APIs
+  generateMusicFromText: (prompt, options = {}) =>
+    ipcRenderer.invoke('generate-music-text', { prompt, options }),
+
+  generateMusicFromImage: (imagePath, options = {}) =>
+    ipcRenderer.invoke('generate-music-image', { imagePath, options }),
+
+  generateMusicFromData: (dataPoints, options = {}) =>
+    ipcRenderer.invoke('generate-music-data', { dataPoints, options }),
+
+  styleTransferAudio: (audioPath, style, options = {}) =>
+    ipcRenderer.invoke('style-transfer-audio', { audioPath, style, options }),
+
+  continueMusic: (seedAudio, options = {}) =>
+    ipcRenderer.invoke('continue-music', { seedAudio, options }),
+
+  // Playback controls
+  playAudio: (audioPath) => ipcRenderer.send('play-audio', audioPath),
+  stopAudio: () => ipcRenderer.send('stop-audio'),
+  pauseAudio: () => ipcRenderer.send('pause-audio'),
+  resumeAudio: () => ipcRenderer.send('resume-audio'),
+
+  // File operations
+  saveAudio: (audioPath, savePath) =>
+    ipcRenderer.invoke('save-audio', { audioPath, savePath }),
+
+  loadAudioFile: () =>
+    ipcRenderer.invoke('load-audio-file'),
+
+  // Event listeners
+  onMusicGenerated: (callback) =>
+    ipcRenderer.on('music-generated', (event, data) => callback(data)),
+
+  onGenerationProgress: (callback) =>
+    ipcRenderer.on('generation-progress', (event, data) => callback(data)),
+
+  onGenerationError: (callback) =>
+    ipcRenderer.on('generation-error', (event, error) => callback(error)),
+
+  onPlaybackUpdate: (callback) =>
+    ipcRenderer.on('playback-update', (event, data) => callback(data)),
+
+  // Cleanup listeners
+  removeAllListeners: () => {
+    ipcRenderer.removeAllListeners('music-generated');
+    ipcRenderer.removeAllListeners('generation-progress');
+    ipcRenderer.removeAllListeners('generation-error');
+    ipcRenderer.removeAllListeners('playback-update');
+  },
 });
